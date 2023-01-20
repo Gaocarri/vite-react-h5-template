38391ca93d9a826f067b920d7daad70366ff1b4c
gaozheng <gaozheng@cvte.com>
2023-01-20 11:22:35 +0800

    feat: remove strict mode, it cause twice render

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./router";
 import { HashRouter } from "react-router-dom";
 import "./index.less";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <HashRouter>
-      <Router />
-    </HashRouter>
-  </React.StrictMode>
+  <HashRouter>
+    <Router />
+  </HashRouter>
 );
